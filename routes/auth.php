2679c88e1e3ab7d7f0bc0a75e3fa7a293e1968ac
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-03-24 14:02:41 +0700

    Change refresh route method from POST to GET in AuthController

--- routes/auth.php
+++ routes/auth.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->controller(AuthController::class)
     ->group(function () {
         Route::post('login', 'login');
-        Route::post('refresh', 'refresh');
+        Route::get('refresh', 'refresh');
         Route::get('me', 'me')->middleware('auth:api');
         Route::post('logout', 'logout')->middleware('auth:api');
     });
